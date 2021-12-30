94e46a60048034062a21f10cfedaac48166b7cb6
virendra-badodiya <89480137+virendra-badodiya@users.noreply.github.com>
2021-12-30 23:01:19 +0530

    Add files via upload

---
+++ src/main/java/com/eazybytes/helloworldservice/RestController.java
@@ -0,0 +1,13 @@
+package com.eazybytes.helloworldservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+	
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello, Welcome to course on Microservices";
+	}
+
+}
